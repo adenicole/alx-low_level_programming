709c27a21727783564056bba3c2fdee5ed5bedec
adenicole <dedoyinnicole@gmail.com>
2022-03-15 07:15:01 -0700

    putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return:0
- */
-
+ *  * main - Prints Putchar
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-char c[10] = "Holberton";
-int i = 0;
-while (i < 9)
-{
-_putchar(c[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
 }

b60467f84f4c006258aa9e6ffeb5ceed40ad9ecf
adenicole <dedoyinnicole@gmail.com>
2022-03-15 05:58:38 -0700

    added new files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+		char c[10] = "_putchar";
+
+		int i = 0;
+
+		while (i < 9)
+		{
+			putchar(c[i]);
+			i++;
+		}
+		putchar('\n');
+		return (0);
+}

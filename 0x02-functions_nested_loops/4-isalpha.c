6d08fd15a464c5dc41e3ae51e85178d163a8522d
adenicole <dedoyinnicole@gmail.com>
2022-03-15 16:03:26 +0100

    4-alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,20 @@
+#include <unistd.h>
+#include <ctype.h>
 #include "main.h"
-
 /**
- * main - check the code.
+ * _isalpha - checks if a character is an alphabet
  *
- * Return: Always 0.
+ * @c: the character to be checked
+ * Return: returns 1 if the character is an alphabet and 0 otherwise
  */
-int main(void)
+int _isalpha(int c)
 {
-    int r;
-
-    r = _isalpha('H');
-    _putchar(r + '0');
-    r = _isalpha('o');
-    _putchar(r + '0');
-    r = _isalpha(108);
-    _putchar(r + '0');
-    r = _isalpha(';');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
